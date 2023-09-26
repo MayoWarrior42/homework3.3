9352bf96066d235d90018d4f42742c14b3934935
MayoWarrior <sweetyl4255.55@gmail.com>
2023-09-26 21:23:54 +0300

    homework

---
+++ homework3.3.1/homework3.3.1/homework3.3.1.cpp
@@ -0,0 +1,83 @@
+﻿
+
+#include <iostream>
+
+
+class Calculator {
+public:
+    double num1;
+    double num2;
+
+    double add(double num1, double num2) {
+        double num3 = num1 + num2;
+        std::cout << "num1 + num2 = " << num3 << "\n";
+        return num3;
+}
+    double multiply(double num1, double num2) {
+        double num3 = num1 * num2;
+        std::cout << "num1 * num2 = " << num3 << "\n";
+        return num3;
+    }
+    double subtract_1_2(double num1, double num2) {
+        double num3 = num1 - num2;
+        std::cout << "num1 - num2 = " << num1 - num2 << "\n";
+        return num3;
+    }
+    double subtract_2_1(double num1, double num2) {
+        double num3 = num2 - num1;
+        std::cout << "num2 - num1 = " << num3 << "\n";
+        return num3;
+    }
+    double divide_1_2(double num1, double num2) {
+        double num3 = num1 / num2;
+        std::cout << "num1 / num2 = " << num3 << "\n";
+        return num3;
+    }
+    double divide_2_1(double num1, double num2) {
+        double num3 = num2 / num1;
+        std::cout << "num2 / num1 = " << num3 << "\n";
+        return num3;
+    }
+    bool set_num1(double num1) {
+        std::cout << "Введите num1: ";
+        std::cin >> num1;
+        this->num1 = num1;
+        if (num1 == 0) {
+            std::cout << "\nНеверный ввод!" << "\n";
+            this->set_num1(num1);
+            return false;
+        }
+        else return true;
+    }
+    bool set_num2(double num2) {
+        std::cout << "Введите num2: ";
+        std::cin >> num2;
+        this->num2 = num2;
+        if (num2 == 0) {
+            std::cout << "\nНеверный ввод!" << "\n";
+            this->set_num2(num2);
+            return false;
+        }
+        return true;
+    }
+};
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    Calculator calc;
+    calc.num1 = 0;
+    calc.num2 = 0;
+    calc.set_num1(calc.num1);
+    calc.set_num2(calc.num2);
+    calc.add(calc.num1, calc.num2);
+    calc.multiply(calc.num1, calc.num2);
+    calc.subtract_1_2(calc.num1, calc.num2);
+    calc.subtract_2_1(calc.num1, calc.num2);
+    calc.divide_1_2(calc.num1, calc.num2);
+    calc.divide_2_1(calc.num1, calc.num2);
+    return 0;
+}
+
---
+++ homework3.3.2/homework3.3.2/homework3.3.2.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <string>
+
+
+class Counter {
+private:
+    int num;
+    std::string val;
+    char input;
+public:
+    Counter()
+    {
+        num = 0;
+    }
+    void set_num() {
+        std::cout << "\nВведите начальное значение счетчика: ";
+        std::cin >> num;
+        this->num;
+    }
+    void inc() {
+        num++;
+    }
+    void dec() {
+        num--;
+    }
+    int get_num() {
+        std::cout << num;
+        return num;
+    }
+    
+    void exit() {
+        std::cout << "\nДо свидания!";
+    }
+
+    void com() {
+        std::cout << "\nВведите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> input;
+        while (input != 'х') {
+            if (input == '+') {
+                this->inc();
+            }
+            else if (input == '-') {
+                this->dec();
+            }
+            else if (input == '=') {
+                this->get_num();
+            }
+            else if (input == 'х') {
+                this->exit();
+            }
+            this->com();
+        }
+    }
+
+    void value() {
+        std::cout << "Вы хотите указать начальное значение счестчика? Введите да или нет: ";
+        std::cin >> val;
+        if (val == "да") {
+            this->set_num();
+            this->com();
+        }
+        else if (val == "нет") {
+            this->com();
+        }
+        else std::cout << "\nОшибка!";
+    }
+};
+
+
+
+int main()
+{
+    system("chcp 1251");
+    Counter count;
+    count.value();
+    return 0;
+}
+
